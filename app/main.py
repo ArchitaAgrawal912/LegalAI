105f7b1e5d31161836caa4c4dbde6a1e8ead597e
ArchitaAgrawal912 <24bec024@nith.ac.in>
2026-05-19 19:04:41 +0530

    Initial FastAPI boilerplate setup

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from app.routes.user_routes import router as user_router
+
+app = FastAPI()
+
+app.include_router(user_router)
+
+
+@app.get("/")
+def home():
+    return {"message": "Server Running"}
---
+++ app/routes/user_routes.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+from app.controllers.user_controller import get_users
+
+router = APIRouter(prefix="/users", tags=["Users"])
+
+
+@router.get("/")
+async def users():
+    return await get_users()
