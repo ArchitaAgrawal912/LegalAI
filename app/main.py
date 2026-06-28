e7e65fa1458a64fe72f24fbda1fbd1409aeb207c
ArchitaAgrawal912 <24bec024@nith.ac.in>
2026-06-29 02:47:40 +0530

    Added cors

--- app/main.py
+++ app/main.py
@@ -49,9 +49,11 @@ def create_app() -> FastAPI:
     app.add_middleware(
         CORSMiddleware,
         allow_origins=[
+            
             "http://localhost:5173",
             "http://127.0.0.1:5173",
             "http://localhost:3000",
+            "http://51.20.84.139",
         ],  
         allow_credentials=True,
         allow_methods=["*"],
